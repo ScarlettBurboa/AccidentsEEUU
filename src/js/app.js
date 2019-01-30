ea516e1cdae9e025b99f759585914f6c15ceccad
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2019-01-30 15:52:59 -0300

    Botón de ejemplo usando módulos

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,18 @@
 const miFuncionTesteable = () => {
+  console.log("Holo, soy una función");
   return true;
 };
 
 const miOtraFunctionTesteable = () => {
   return false;
 };
+
+
+// Importante agregar la función que se conectará con el dom desde javascript
+// O no funcionará
+if( document ) {
+  document.getElementById('miBoton').addEventListener('click', miFuncionTesteable);
+}
 
 export const miFuncionTesteableExp = miFuncionTesteable;
 export const miOtraFunctionTesteableExp = miOtraFunctionTesteable;
